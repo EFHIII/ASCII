a5ae3cd3a536aab28aa7d79a2a6d93a860ce3ffc
Edward Haas <efhiii2000@gmail.com>
2021-06-04 03:34:21 -0700

    add new modes

--- main.js
+++ main.js
@@ -53,10 +53,13 @@ P = [
 
 R = a => (a - d) * 255 / (255 - d - (255 - b));
 
-function dither(x, y, v) {
-  n = R(v) / 256;
-  n = Math.pow(n, 2 / 3);
-  var mat = [
+function dither(x, y, v, sh) {
+  n = R(v) / 255;
+  if(sh){
+    n = Math.pow(n, 2 / 3);
+    n = (n+64/255)*(255-64)/255;
+  }
+  let mat = [
     [1, 33, 13, 45, 3, 35, 15, 47],
     [59, 17, 49, 25, 57, 19, 51, 27],
     [9, 41, 5, 37, 11, 43, 7, 39],
@@ -67,17 +70,17 @@ function dither(x, y, v) {
     [12, 44, 8, 40, 10, 42, 6, 38],
     [54, 32, 64, 24, 56, 30, 62, 22],
   ];
-  if(n > mat[x % 8][y % 8] / 64) {
+  if(n > (mat[x % 8][y % 8]-0.5) / 64) {
     return 1;
   }
   return 0;
 };
 
-var brail = "⠂⠁⠂⠃⠄⠅⠆⠇⠈⠉⠊⠋⠌⠍⠎⠏⠐⠑⠒⠓⠔⠕⠖⠗⠘⠙⠚⠛⠜⠝⠞⠟⠠⠡⠢⠣⠤⠥⠦⠧⠨⠩⠪⠫⠬⠭⠮⠯⠰⠱⠲⠳⠴⠵⠶⠷⠸⠹⠺⠻⠼⠽⠾⠿⡀⡁⡂⡃⡄⡅⡆⡇⡈⡉⡊⡋⡌⡍⡎⡏⡐⡑⡒⡓⡔⡕⡖⡗⡘⡙⡚⡛⡜⡝⡞⡟⡠⡡⡢⡣⡤⡥⡦⡧⡨⡩⡪⡫⡬⡭⡮⡯⡰⡱⡲⡳⡴⡵⡶⡷⡸⡹⡺⡻⡼⡽⡾⡿⢀⢁⢂⢃⢄⢅⢆⢇⢈⢉⢊⢋⢌⢍⢎⢏⢐⢑⢒⢓⢔⢕⢖⢗⢘⢙⢚⢛⢜⢝⢞⢟⢠⢡⢢⢣⢤⢥⢦⢧⢨⢩⢪⢫⢬⢭⢮⢯⢰⢱⢲⢳⢴⢵⢶⢷⢸⢹⢺⢻⢼⢽⢾⢿⣀⣁⣂⣃⣄⣅⣆⣇⣈⣉⣊⣋⣌⣍⣎⣏⣐⣑⣒⣓⣔⣕⣖⣗⣘⣙⣚⣛⣜⣝⣞⣟⣠⣡⣢⣣⣤⣥⣦⣧⣨⣩⣪⣫⣬⣭⣮⣯⣰⣱⣲⣳⣴⣵⣶⣷⣸⣹⣺⣻⣼⣽⣾⣿";
+let brail = "⠠⠁⠂⠃⠄⠅⠆⠇⠈⠉⠊⠋⠌⠍⠎⠏⠐⠑⠒⠓⠔⠕⠖⠗⠘⠙⠚⠛⠜⠝⠞⠟⠠⠡⠢⠣⠤⠥⠦⠧⠨⠩⠪⠫⠬⠭⠮⠯⠰⠱⠲⠳⠴⠵⠶⠷⠸⠹⠺⠻⠼⠽⠾⠿⡀⡁⡂⡃⡄⡅⡆⡇⡈⡉⡊⡋⡌⡍⡎⡏⡐⡑⡒⡓⡔⡕⡖⡗⡘⡙⡚⡛⡜⡝⡞⡟⡠⡡⡢⡣⡤⡥⡦⡧⡨⡩⡪⡫⡬⡭⡮⡯⡰⡱⡲⡳⡴⡵⡶⡷⡸⡹⡺⡻⡼⡽⡾⡿⢀⢁⢂⢃⢄⢅⢆⢇⢈⢉⢊⢋⢌⢍⢎⢏⢐⢑⢒⢓⢔⢕⢖⢗⢘⢙⢚⢛⢜⢝⢞⢟⢠⢡⢢⢣⢤⢥⢦⢧⢨⢩⢪⢫⢬⢭⢮⢯⢰⢱⢲⢳⢴⢵⢶⢷⢸⢹⢺⢻⢼⢽⢾⢿⣀⣁⣂⣃⣄⣅⣆⣇⣈⣉⣊⣋⣌⣍⣎⣏⣐⣑⣒⣓⣔⣕⣖⣗⣘⣙⣚⣛⣜⣝⣞⣟⣠⣡⣢⣣⣤⣥⣦⣧⣨⣩⣪⣫⣬⣭⣮⣯⣰⣱⣲⣳⣴⣵⣶⣷⣸⣹⣺⣻⣼⣽⣾⣿";
 
 C = P[0];
 
-var Brail = false;
+let Brail = false;
 
 function escapeHtml(unsafe) {
   return unsafe
@@ -92,7 +95,7 @@ function escapeHtml(unsafe) {
 p = a => new Promise((r) => { setTimeout(r, 1e3) });
 D = a => document.getElementById(a);
 
-var defaultUrl = { url: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/80/Wikipedia-logo-v2.svg/1200px-Wikipedia-logo-v2.svg.png', size: 200, ratio: 0.82, type:"0" };
+let defaultUrl = { url: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/80/Wikipedia-logo-v2.svg/1200px-Wikipedia-logo-v2.svg.png', size: 200, ratio: 0.82, type:"0" };
 
 function setUrl() {
   if(!history.pushState) { return; }
@@ -106,7 +109,7 @@ function setUrl() {
       qs += el + '=' + encodeURIComponent(element.value);
     }
   }
-  var newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + qs;
+  let newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + qs;
   window.history.pushState({ path: newurl }, '', newurl);
 }
 
@@ -114,12 +117,12 @@ function drawBrail(w, h, a) {
   let t = '';
   for(i = 0; i < h - 2; i += 4) {
     for(j = 0; j < w - 1; j += 2) {
-      var bin = '';
-      bin += dither(j + 1, i + 3, a[((i + 3) * (w >> 0) + j + 1) * 4]) ? 0 : 1;
-      bin += dither(j, i + 3, a[((i + 3) * (w >> 0) + j + 1) * 4]) ? 0 : 1;
-      for(var bx = 1; bx >= 0; bx--) {
-        for(var by = 2; by >= 0; by--) {
-          bin += dither(j + bx, i + by, a[((i + by) * (w >> 0) + j + bx) * 4]) ? 0 : 01;
+      let bin = '';
+      bin += dither(j + 1, i + 3, a[((i + 3) * (w >> 0) + j + 1) * 4], true) ? 0 : 1;
+      bin += dither(j, i + 3, a[((i + 3) * (w >> 0) + j + 1) * 4], true) ? 0 : 1;
+      for(let bx = 1; bx >= 0; bx--) {
+        for(let by = 2; by >= 0; by--) {
+          bin += dither(j + bx, i + by, a[((i + by) * (w >> 0) + j + bx) * 4], true) ? 0 : 01;
         }
       }
       t += brail[parseInt(bin, 2)];
@@ -129,19 +132,138 @@ function drawBrail(w, h, a) {
   return t;
 }
 
+function floyd(colorDepth, tempData, width, height) {
+  console.log([width,height]);
+  let ans=tempData.slice();
+
+  let maxV = colorDepth / 255;
+
+  let mem = [[],[]];
+  for (let i = 0; i < width; i++) {
+    mem[0][i] = 4/16+tempData[i*4]*maxV;
+    //mem[0][i] = Math.min(colorDepth,Math.random()*colorDepth/2 + tempData[i]*maxV);
+    mem[1][i] = tempData[(i+width)*4]*maxV;
+  }
+  //mem[1][0] += (Math.random()*4 + 4) / 16;
+  mem[1][0] += 4 / 16;
+
+  let y = 0;
+  while (y < height) {
+    for (let x = 0; x < width; x++) {
+      let oldp = mem[0][x];
+      let newp = Math.max(0,Math.min(colorDepth,Math.round(oldp>>0)));//Math.max(0,Math.min(colorDepth,Math.round(oldp)));
+      let err = oldp-newp;//oldp - newp;
+      mem[0][x + 1] += Math.max(0,Math.min(colorDepth,err * 7 / 16));
+      mem[1][x - 1] += Math.max(0,Math.min(colorDepth,err * 3 / 16));
+      mem[1][x] += Math.max(0,Math.min(colorDepth,err * 5 / 16));
+      mem[1][x + 1] += Math.max(0,Math.min(colorDepth,err * 1 / 16));
+    }
+    if (y > 0) {
+      for (let x = 0; x < width; x++) {
+        ans[x + (y - 1) * width] = Math.max(0,Math.min(colorDepth,mem[0][x] >> 0));
+        //ans[x + (y - 1) * width] = Math.max(0,mem[0][x]>>0);//Math.max(0,Math.min(colorDepth,(Math.round(mem[0][x]))));
+      }
+    }
+    mem.reverse();
+    for (let x = 0; x < width; x++) {
+      mem[1][x] = tempData[(x + (y + 1) * width)*4]*maxV;
+    }
+    mem[1][0] += 4 / 16;
+    y++;
+  }
+  for (let x = 0; x < width; x++) {
+    ans[x + (y - 1) * width] =  Math.max(0,Math.min(colorDepth,mem[0][x] >> 0));
+  }
+  return ans;
+}
+
+let sv=[];
+
+function drawBoxGradient(w, h, a) {
+  const palette = ' ▏▎▍▌▋▊▉█';
+
+  a=a.map(a=>(a/255)*(a/255)*255);
+
+  let t = '';
+  for(i = 0; i < h-1; i ++) {
+    for(j = 0; j < w; j ++) {
+      if(invert){
+        t += palette[(a[(j+i*w)*4]/255*(palette.length-2)>>0)+1-dither(i,j,((a[(j+i*w)*4]/255*(palette.length-2))%1)*255)];
+      }
+      else{
+        t += palette[palette.length-2-(a[(j+i*w)*4]/255*(palette.length-2)>>0)+1-dither(i,j,((a[(j+i*w)*4]/255*(palette.length-2))%1)*255)];
+      }
+    }
+    t += '\n';
+  }
+  return t;
+}
+
+function drawBoxDevision(w, h, a) {
+  const blocks = " ▖▗▄▘▌▚▙▝▞▐▟▀▛▜█";
+
+  a=a.map(a=>(a/255)*(a/255)*255);
+
+  let t = '';
+  for(i = 0; i < h-3; i +=2) {
+    for(j = 0; j < w-1; j +=2) {
+      t += blocks[
+        15-dither(i,j+1,a[(1+j+(i)*w)*4])-
+        dither(i+1,j+1,a[(1+j+(1+i)*w)*4])*2-
+        dither(i,j,a[(j+(i)*w)*4])*4-
+        dither(i+1,j,a[(j+(1+i)*w)*4])*8];
+    }
+    t += '\n';
+  }
+  return t;
+}
+
+function drawBoxGradientFloyd(w, h, a) {
+  const palette = ' ▏▎▍▌▋▊▉█';
+
+  let newData = floyd(palette.length-1, a.map(a=>(a/255)*(a/255)*255), w*1, h*1);
+
+  sv=newData;
+
+  let t = '';
+  for(i = 0; i < h-1; i ++) {
+    for(j = 0; j < w; j ++) {
+      t += palette[newData[j+i*w]];
+    }
+    t += '\n';
+  }
+  return t;
+}
+function drawBoxDevisionFloyd(w, h, a) {
+  const blocks = " ▖▗▄▘▌▚▙▝▞▐▟▀▛▜█";
+
+  let newData = floyd(1, a.map(a=>(a/255)*(a/255)*255), w*1, h*1);
+
+  sv=newData;
+
+  let t = '';
+  for(i = 0; i < h-3; i +=2) {
+    for(j = 0; j < w-1; j +=2) {
+      t += blocks[newData[1+j+(i)*w]+newData[1+j+(1+i)*w]*2+newData[j+(i)*w]*4+newData[j+(1+i)*w]*8];
+    }
+    t += '\n';
+  }
+  return t;
+}
+
 async function ASCII(url, size) {
-  D('v').innerHTML = `<img src="${url}" id="s"><canvas id="i">`
-  D = a => document.getElementById(a);
-  c = D('i');
-  X = c.getContext('2d');
+  D('v').innerHTML = `<img src="${url}" id="s"><canvas id="i">`;
   s = document.getElementById('s');
   s.crossOrigin = "Anonymous";
+  c = D('i');
+  X = c.getContext('2d');
   await p();
   W = c.width = size;
-  H = c.height = size * s.height / s.width * Ratio * (Brail ? 1.35 : 1);
-  X.filter = "grayscale(100) contrast(1)";
+  H = c.height = size * s.height / s.width * Ratio * (Brail ? 1.35 : 1) * (DV > 3 ? 0.5 : 1);
+  X.filter = "grayscale(100)";
   X.drawImage(s, 0, 0, W, H);
   a = X.getImageData(0, 0, W, H).data;
+
   t = '';
   // $@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`'.
   // gbv,._
@@ -161,6 +283,19 @@ async function ASCII(url, size) {
   }
   if(Brail) {
     return drawBrail(W, H, a);
+  }
+
+  if(DV == 4){
+    return drawBoxGradient(W, H, a);
+  }
+  if(DV == 5){
+    return drawBoxDevision(W, H, a);
+  }
+  if(DV == 6){
+    return drawBoxGradientFloyd(W, H, a);
+  }
+  if(DV == 7){
+    return drawBoxDevisionFloyd(W, H, a);
   }
   for(i = 0; i < H - 1; i += 2) {
     for(j = 0; j < W; j++) {
